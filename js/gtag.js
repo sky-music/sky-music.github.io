29749ec8acd0557fb5702f6d606b42439206920d
jmmelko <jmmelko@gmail.com>
2020-07-25 21:34:02 +0200

    Delete gtag.js

--- js/gtag.js
+++
@@ -1,8 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-173568580-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-173568580-1');
-</script>
