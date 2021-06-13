54cd569adf9600f0e0cda4b7588e3c8d928f86d3
jmmelko <jmmelko@gmail.com>
2021-06-13 04:07:17 +0200

    Update navigationTableScript.js

--- js/navigationTableScript.js
+++ js/navigationTableScript.js
@@ -4,7 +4,7 @@ window.addEventListener("load", function() {
     let navigationTable = document.createElement("table");
     navigationTable.id = "navigation";
     
-    tableInsideHTML = '<tr><td><a href="/index.html"><img src="/assets/images/sky-music-logo-sheets.png" width="100" height="100" /></a></td>' +
+    tableInsideHTML = '<tr><td><a href="/index.html"><img src="/assets/images/sky-music-logo.png" width="100" height="100" /></a></td>' +
                       '<td><h1><a href="/index.html">Sky Music</a></h1></td></tr>';
 
 
